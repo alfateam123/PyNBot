de2c165d0e2aed2f354f5a01ee6b54698cacd159
nicolapcweek94 <wasp@nwa.xyz>
2015-10-26 00:40:16 +0100

    First commit with lastfm command

--- src/pynbot.py
+++ src/pynbot.py
@@ -10,15 +10,19 @@ lastfm_apisecret = config["lastfm_apisecret"]
 bot = telebot.TeleBot(TOKEN)
 
 def lastfm_parse(message):
-    lastfm = pylast.LastFMNetwork(lastfm_apikey, lastfm_apisecret)
-    message_arguments = message.text.split(" ")
-    lastfm_username = message_arguments[1] if len(message_arguments) == 2 else (message.from_user.username if message.from_user.username != None and message.from_user.username != "" else "citizenwasp")
-    current_track = lastfm.get_user(lastfm_username).get_now_playing()
+    try:
+        lastfm = pylast.LastFMNetwork(lastfm_apikey, lastfm_apisecret)
+        message_arguments = message.text.split(" ")
+        lastfm_username = message_arguments[1] if len(message_arguments) == 2 else (message.from_user.username if message.from_user.username != None and message.from_user.username != "" else "citizenwasp")
+        current_track = lastfm.get_user(lastfm_username).get_now_playing()
 
-    if (current_track == None or current_track == ""):
-        current_track = lastfm_username + " is not scrobbling!"
+        if (current_track == None or current_track == ""):
+            current_track = lastfm_username + " is not scrobbling!"
 
-    return current_track
+        return current_track
+    except Exception as inst:
+        print(type(inst))
+        return "Unexpected error"
 
 
 @bot.message_handler(commands=["lastfm"])
